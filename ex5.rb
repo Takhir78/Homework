805e8c5e1ae41e9bc8b6d52b12199be5799713eb
Takhir78 <t.salimov@gmail.com>
2017-02-15 17:01:08 -0500

    update

--- ex5.rb
+++ ex5.rb
@@ -1,7 +1,7 @@
 puts "Let's talk about #{'Takhir'}."
 puts my_age = 39 # 2017-1978 huh too old :((
 puts "He's #{'6'} inches tall."
-puts "He's #{'200'} pounds heavy."
+puts "He's #{'220'} pounds heavy."
 puts "Actually that's not too heavy."
 puts "He's got #{'green'} eyes and #{'black'} hair."
 puts "His teeth are usually #{'like pearl'} depending on the coffee."
